a9cdb45d42ddeccba0fccc95033951c3cd23264a
Abhishek <abhishek.x7x@gmail.com>
2020-11-24 12:54:11 +0530

    Abhishek lead first commit

---
+++ GitProject/src/main/java/com/jlcindia/GitProject/App.java
@@ -0,0 +1,13 @@
+package com.jlcindia.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
